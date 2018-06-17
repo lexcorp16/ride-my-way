2cd08f0fd5bd9d7a5850e06bc26cb73b048d719b
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-06-17 22:10:59 +0100

    [Chore #158414234] Add test for all ride offers endpoint

--- test/api.test.js
+++ test/api.test.js
@@ -1,9 +1,21 @@
+/* eslint no-unused-expressions: 0 */
+
 import chai from 'chai';
+import request from 'supertest';
+
+import app from '../index';
 
 const { expect } = chai;
 
-describe('Sample Test', () => {
-  it('Adds one and two', () => {
-    expect(1 + 2).to.equal(3);
+describe('API tests', () => {
+  it('Gets all rides', (done) => {
+    request(app)
+      .get('/api/v1/rides')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.response).to.not.be.undefined;
+        done();
+      });
   });
 });
