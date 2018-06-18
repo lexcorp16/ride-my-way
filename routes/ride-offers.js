74896706c7a2fd015a5452f9a59d54bddf4741e2
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-06-18 01:36:12 +0100

    [Feature #158415512] Add endpoint to get single ride offer

--- routes/ride-offers.js
+++ routes/ride-offers.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import getAllRides from '../controllers/ride-offers';
+import * as rideController from '../controllers/ride-offers';
 
 const router = express.Router();
 
-router.get('/api/v1/rides', getAllRides);
+router.get('/api/v1/rides', rideController.getAllRides);
+
+router.get('/api/v1/rides/:rideId', rideController.getOneRide);
 
 export default router;
