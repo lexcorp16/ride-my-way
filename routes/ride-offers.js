abaac858c5d39c7762086b60e7d964f8dc746a47
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-06-20 01:19:26 +0100

    [Feature #158475808] Add api index page

--- routes/ride-offers.js
+++ routes/ride-offers.js
@@ -4,16 +4,16 @@ import * as rideController from '../controllers/ride-offers';
 
 const router = express.Router();
 
-router.get('/api/v1/rides', rideController.getAllRides);
+router.get('/v1/rides', rideController.getAllRides);
 
-router.get('/api/v1/rides/:rideId', rideController.getOneRide);
+router.get('/v1/rides/:rideId', rideController.getOneRide);
 
-router.get('/api/v1/rides/:rideId/requests', rideController.getOfferRequests);
+router.get('/v1/rides/:rideId/requests', rideController.getOfferRequests);
 
-router.post('/api/v1/rides', rideController.createRideOffer);
+router.post('/v1/rides', rideController.createRideOffer);
 
-router.post('/api/v1/rides/:rideId/requests', rideController.joinRide);
+router.post('/v1/rides/:rideId/requests', rideController.joinRide);
 
-router.post('/api/v1/rides/:rideId/requests/:requestId', rideController.respondToRideRequest);
+router.post('/v1/rides/:rideId/requests/:requestId', rideController.respondToRideRequest);
 
 export default router;
