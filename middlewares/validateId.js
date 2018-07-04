47f0ba67e2e893d2f77785099573e4f7f4f5ff5d
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-07-04 21:00:22 +0100

    Return in middleware

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -2,7 +2,7 @@ const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[
 
 const validateId = (req, res, next) => {
   if (!uuidRegex.test(req.params.rideId)) {
-    res.status(400).send({
+    return res.status(400).send({
       status: 'failed',
       message: 'ID supplied is invalid',
     });
