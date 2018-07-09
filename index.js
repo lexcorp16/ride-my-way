57ba513e89e2cf1cc5d1b3b7e9fcc7e72fb4d660
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-07-09 22:58:07 +0100

    Enable cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
