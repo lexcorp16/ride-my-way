fc9ef9e2cbae7fbb011c171a4d76333423747687
Fasoro-Joseph <a.fasorojoseph@jaramobile.com>
2018-07-17 21:35:37 +0100

    Update ride offer date validation

--- middlewares/validateDateTime.js
+++ middlewares/validateDateTime.js
@@ -4,7 +4,7 @@ const timeRegex = /^\d{1,2}:\d{2}([ap]m)?$/;
 const validateDateTime = (req, res, next) => {
   const { departureTime, departureDate } = req.body;
 
-  if (!dateRegex.test(departureDate)) {
+  if (!dateRegex.test(departureDate) || +departureDate.split('/')[0] > 12) {
     return res.status(400).send({
       status: 'failed',
       message: 'Please enter a date in this format mm/dd/yyyy',
@@ -13,6 +13,11 @@ const validateDateTime = (req, res, next) => {
     return res.status(400).send({
       status: 'failed',
       message: 'Please enter a time in this format hh:mm',
+    });
+  } else if (Date.parse(departureDate) < +new Date()) {
+    return res.status(400).send({
+      status: 'failed',
+      message: 'Please enter a date in the future.',
     });
   }
 
